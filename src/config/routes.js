5df0c395cb6347ee50a2577ffe3eeadbfc1bc51e
geekman2021 <rakotonirinaarnaud108@gmail.com>
2023-03-10 12:50:07 +0300

    change the git branch

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,6 +2,11 @@ import CreateChallengePage from "../components/CreateChallengePage/CreateChallen
 import HomePage from "../modules/HomePage/HomePage.component";
 import Login from "../modules/Login/Login.component";
 import ProfilePage from "../modules/ProfilePage/NewProfilePage/Profile.Page";
+<<<<<<< Updated upstream
+=======
+import PagesChalenges from "../modules/PagesChalenges";
+import EventsPage from "../modules/EventsPage";
+>>>>>>> Stashed changes
 
 const ROUTES = [
   {
@@ -24,6 +29,19 @@ const ROUTES = [
     path: "/profile",
     element: <ProfilePage />,
   },
+<<<<<<< Updated upstream
+=======
+  {
+    title: "challenge",
+    path: "/challenge",
+    element: <PagesChalenges />,
+  },
+  {
+    title: "event",
+    path: "/Event",
+    element: <EventsPage />,
+  },
+>>>>>>> Stashed changes
 ];
 
 export default ROUTES;
