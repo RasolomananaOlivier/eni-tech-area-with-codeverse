0808f2214dcdc37e0d9f9d3c049b5a0a0d2c8da1
Rasolomanana Herimanitra Olivier <herimanitraolivierr@gmail.com>
2023-03-10 08:36:03 +0300

    feat: add apis

--- src/api/answersApi.js
+++ src/api/answersApi.js
@@ -1,38 +1,9 @@
 import axios from "axios";
+import { getAnswersUrl } from "./urls";
 
-import {
-  allAnswersData as _allAnswersData,
-  createSingleAnswer as _createSingleAnswer,
-  deleteSingleAnswer as _deleteSingleAnswer,
-  allAnswersComment as _allAnswersComment,
-} from "./urls";
-
-export const allAnswersData = (questionId) => {
-  return axios.get(_allAnswersData.replace("{questionId}", questionId));
-};
-
-export const allAnswerCommets = (questionId, answerId) => {
-  return axios.get(
-    _allAnswersComment
-      .replace("{questionId}", questionId)
-      .replace("{answerId}", answerId)
+export const getAnswers = async (questionId) => {
+  const res = await axios.get(
+    getAnswersUrl.replace("{questionId}", questionId)
   );
-};
-
-export const createSingleAnswer = (postId, formData) => {
-  const config_headers = {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
-
-  return axios.post(
-    _createSingleAnswer.replace("{postId}", postId),
-    formData,
-    config_headers
-  );
-};
-
-export const deleteSingleAnswer = (AnswerId) => {
-  return axios.delete(_deleteSingleAnswer.replace("{AnswerId}", AnswerId));
+  return res.data;
 };
--- src/api/commentsApi.js
+++ src/api/commentsApi.js
@@ -1,25 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
+import { getCommentsUrl } from "./urls";
 
-import {
-  allCommentsData as _allCommentsData,
-  createSingleComment as _createSingleComment,
-  deleteSingleComment as _deleteSingleComment
-} from './urls';
-
-export const allCommentsData = (id) => {
-  return axios.get(_allCommentsData.replace('{id}', id));
-}
-
-export const createSingleComment = (postId, formData) => {
-  const config_headers = {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
-
-  return axios.post(_createSingleComment.replace('{postId}', postId), formData, config_headers);
-}
-
-export const deleteSingleComment = (CommentId) => {
-  return axios.delete(_deleteSingleComment.replace('{CommentId}', CommentId));
-}
+export const getComments = async (questionId, answerId) => {
+  const res = await axios.get(
+    getCommentsUrl
+      .replace(":questionId", questionId)
+      .replace(":answerId", answerId)
+  );
+  return res.data;
+};
